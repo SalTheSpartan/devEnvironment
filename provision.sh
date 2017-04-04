71a8f1ed91644c5f1282dbcf8aac6daad836f9fb
Salman Khan <skhan@spartaglobal.co>
2017-04-04 16:40:00 +0100

    added node stuff

--- provision.sh
+++ provision.sh
@@ -3,3 +3,11 @@ apt-get -y update
 apt-get -y install nginx
 
 service nginx start
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+
+sudo npm install pm2 -g
+
+sudo apt-get install git-core -y
