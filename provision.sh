ec21608030133f542a9cb84eafc2bf38e1de13fd
Salman Khan <skhan@spartaglobal.co>
2017-04-06 10:46:37 +0100

    updated bash src to add github ssh key

--- provision.sh
+++ provision.sh
@@ -17,3 +17,6 @@ sudo rm /etc/nginx/sites-available/default
 sudo cp nginx.default /etc/nginx/sites-available/default
 
 sudo  service nginx restart
+
+echo "eval `ssh-agent`" >> ~/.bashrc
+echo "ssh-add ~/.ssh/github"  >> ~/.bashrc  
